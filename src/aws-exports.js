f03dde8d145ad2241646c742eda1ab9a2fbb6188
Baba <fj0363cb@aa.jp.fujitsu.com>
2025-03-03 17:14:11 +0900

    Added required files for deployment

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+    aws_project_region: "ap-northeast-1",
+    aws_cognito_region: "ap-northeast-1",
+    aws_user_pools_id: "ap-northeast-1_h2Ira36fY",
+    aws_user_pools_web_client_id: "128mcrh4ftsd1onp7q9vomaolp",
+    oauth: {}
+};
+
+export default awsmobile;
